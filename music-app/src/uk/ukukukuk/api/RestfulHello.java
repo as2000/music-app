ce0d13b208cb2793e4473cf3a8758d504a95563d
aiden <aidenscott12@googlemail.com>
2016-08-22 17:45:48 +0100

    hello world restful

---
+++ music-app/src/uk/ukukukuk/api/RestfulHello.java
@@ -0,0 +1,24 @@
+package uk.ukukukuk.api;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+
+@Path("song")
+public class RestfulHello {
+
+	@GET
+	@Path("/{param}")
+	public Response printMessage(@PathParam("param") String msg) {
+
+		String result = "Restful example : " + msg;
+
+		return Response.status(200).entity(result).build();
+
+	}
+	
+	
+}

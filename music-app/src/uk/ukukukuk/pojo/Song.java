b84c126962f3e213b61ba58cee39fa7c9080bf3c
aiden <aidenscott12@googlemail.com>
2016-08-22 12:42:56 +0100

    create song pojo

---
+++ music-app/src/uk/ukukukuk/pojo/Song.java
@@ -0,0 +1,26 @@
+package uk.ukukukuk.pojo;
+
+public class Song {
+
+	private String name;
+	private String artist;
+	private String musicBrainzID;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	public String getMusicBrainzID() {
+		return musicBrainzID;
+	}
+	public void setMusicBrainzID(String musicBrainzID) {
+		this.musicBrainzID = musicBrainzID;
+	}
+}

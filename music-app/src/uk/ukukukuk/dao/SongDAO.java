34177fc75bc185a23cb701b26f4f4689204aadf0
aiden <aidenscott12@googlemail.com>
2016-08-22 18:18:37 +0100

    use entity manager to find a song

--- music-app/src/uk/ukukukuk/dao/SongDAO.java
+++ music-app/src/uk/ukukukuk/dao/SongDAO.java
@@ -2,6 +2,13 @@ package uk.ukukukuk.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.ParameterExpression;
+import javax.persistence.criteria.Root;
+
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
@@ -27,6 +34,24 @@ public class SongDAO {
 	public static List<Song> getAllSongs() {
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		return session.createCriteria(Song.class).list();
+	}
+	
+	public static List<Song> getSong(long id){
+		EntityManager em = HibernateUtil.getSessionFactory().createEntityManager();
+		CriteriaBuilder cb = HibernateUtil.getSessionFactory().getCriteriaBuilder();
+		CriteriaQuery<Song> q = cb.createQuery(Song.class);
+		Root<Song> song = q.from(Song.class);
+		ParameterExpression<Long> parameter = cb.parameter(Long.class);
+		q.select(song).where(cb.equal(song.get("id"), parameter));
+		
+		TypedQuery<Song> query = em.createQuery(q);
+		query.setParameter(parameter, id);
+		List<Song> results = query.getResultList();
+		
+		
+		return results;
+		
+		
 	}
 
 }
